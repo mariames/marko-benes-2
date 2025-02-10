30a1cee922b2a742ec4da87d05c5a73bf0b597ec
marijanadev <marijanavujovic92@gmail.com>
2025-02-10 06:20:24 +0100

    Service has been updated

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Button } from "./ui/MovingBorder";
 
 const services = [
   "Labels and Packaging",
@@ -15,29 +14,25 @@ const services = [
 
 const Services: React.FC = () => {
   return (
-  <div className="bg-gray-950 py-10">
+    <div className="bg-gray-950 py-10">
       <p className="text-5xl text-center my-10 pt-4 pb-10 text-gray-300">
         Services
       </p>
-    <div className="grid grid-cols-3 gap-10 p-6">
-
-      {services.map((service, index) => (
-        <Button
-              borderRadius="1.75rem"
-              duration={5000}
-              className="h-32 flex items-center justify-center bg-gray-900  rounded-lg border border-slate-800"
-              key={index}
+      <div className="grid grid-cols-3 gap-10 p-6">
+        {services.map((service, index) => (
+          <button
+            className="h-32 flex items-center justify-center bg-gray-900 rounded-lg border border-slate-800 transform transition-all duration-300 hover:scale-105 hover:shadow-[0_5px_10px_rgba(0,255,255,0.6),0_5px_15px_rgba(255,0,255,0.6)]"
+            key={index}
+          >
+            <span
+              className="bg-clip-text text-2xl text-transparent bg-gradient-to-r from-cyan-500 to-teal-500 font-semibold text-center p-6 cursor-pointer"
             >
-        <span
-          key={index}
-          className=" bg-clip-text text-2xl text-transparent bg-gradient-to-r from-cyan-500 to-teal-500 font-semibold text-center p-6 cursor-pointer"
-        >
-          {service}
-          </span>
-        </Button>
-      ))}
+              {service}
+            </span>
+          </button>
+        ))}
+      </div>
     </div>
-  </div>
   );
 };
 

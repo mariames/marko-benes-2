3307ddaeab918ec49e65edadc08e394e0dc5d57d
marijanadev <marijanavujovic92@gmail.com>
2025-02-10 06:08:09 +0100

    Services

--- components/Services.tsx
+++ components/Services.tsx
@@ -4,19 +4,19 @@ import { Button } from "./ui/MovingBorder";
 const services = [
   "Labels and Packaging",
   "Posters and Flyers",
-  "Billboards & Roll Ups",
+  "Billboards, Banners & Roll Ups",
   "Vehicle Branding",
-  "Promo Booths",
-  "Shelves",
+  "Trade Promo Booths",
+  "Promo Shelves",
   "Logo",
-  "T-shirts",
-  "Video Animations",
+  "T-Shirt design",
+  "Video Design",
 ];
 
 const Services: React.FC = () => {
   return (
-    <>
-      <p className="text-5xl text-center my-10">
+  <div className="bg-gray-950 py-10">
+      <p className="text-5xl text-center my-10 pt-4 pb-10 text-gray-300">
         Services
       </p>
     <div className="grid grid-cols-3 gap-10 p-6">
@@ -24,19 +24,20 @@ const Services: React.FC = () => {
       {services.map((service, index) => (
         <Button
               borderRadius="1.75rem"
-              className="h-32 flex items-center justify-center bg-gray-100  rounded-lg"
+              duration={5000}
+              className="h-32 flex items-center justify-center bg-gray-900  rounded-lg border border-slate-800"
               key={index}
             >
         <span
           key={index}
-          className=" bg-clip-text text-transparent bg-gradient-to-r from-cyan-600 to-teal-600 font-semibold text-center p-6 cursor-pointer"
+          className=" bg-clip-text text-2xl text-transparent bg-gradient-to-r from-cyan-500 to-teal-500 font-semibold text-center p-6 cursor-pointer"
         >
           {service}
           </span>
         </Button>
       ))}
     </div>
-    </>
+  </div>
   );
 };
 

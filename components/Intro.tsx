0e6a553e04a14de8dfc0ceb89c70028f3a124ba5
marijanadev <marijanavujovic92@gmail.com>
2025-02-11 19:44:28 +0100

    Intro- image has been resized

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -7,11 +7,11 @@ const Intro: React.FC = () => {
   return (
     <div className="flex flex-col md:flex-row items-center md:items-start py-10 px-5">
       {/* Left side - Text */}
-      <div className="md:w-1/3 mb-8 md:mb-0 flex justify-center mt-3">
+      <div className="md:w-1/3 mb-8 md:mb-0 flex justify-center mt-2">
         <img
           src="/hero_image.png" // replace with your image path
           alt="Sample"
-          className="w-auto h-96 rounded-lg transform transition-all duration-700 ease-in-out hover:scale-105"
+          className="w-auto h-80 rounded-lg transform transition-all duration-700 ease-in-out hover:scale-105"
         />
       </div>
 

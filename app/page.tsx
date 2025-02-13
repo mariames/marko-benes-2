85eb9c7f497f3431c56909768295d7fa4aecc9c7
marijanadev <marijanavujovic92@gmail.com>
2025-02-13 12:54:57 +0100

    Logoi

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { Navbar } from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import { InfoCards } from "@/components/InfoCard";
 import Competitors from "@/components/Competitors";

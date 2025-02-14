1fd209e26e0eb017a6f632cdd7a5c49ce780ed5f
marijanadev <marijanavujovic92@gmail.com>
2025-02-14 18:48:55 +0100

    Imrpovements

--- app/services/labels_and_packaging/page.tsx
+++ app/services/labels_and_packaging/page.tsx
@@ -4,17 +4,17 @@ import React from 'react'
 const page = () => {
   return (
     <>
+    {/** 
       <div className='flex justify-center christmas-title text-5xl my-16'>
         Labels & Packaging
       </div>
         <div className="container mx-auto p-6">
-      {/* Text Section */}
       <div className="text-center mb-10">
         <h1 className="text-4xl font-semibold mb-4">
           Are you in need of an eye-catching and professionally designed label and packaging for your product?
         </h1>
         <p className="text-lg text-gray-600 mb-4">
-          I'm here to help you create an impressive design that will captivate your target audience and elevate your brand. My
+          I am here to help you create an impressive design that will captivate your target audience and elevate your brand. My
           approach combines creativity and strategic thinking to make your product stand out on the shelf.
         </p>
         <p className="text-lg text-gray-600 mb-8">
@@ -23,7 +23,7 @@ const page = () => {
         </p>
       </div>
 
-      {/* Image Gallery Section */}
+      
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
           <img src="https://via.placeholder.com/400" alt="Packaging 1" className="w-full h-64 object-cover rounded-lg mb-4" />
@@ -48,6 +48,7 @@ const page = () => {
       </div>
       <Faq />
     </div>
+    */}
     </>
 
   )

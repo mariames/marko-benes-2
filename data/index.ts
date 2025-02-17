a790a4118eed91b03cd141ed0bd18d083f354dce
marijanadev <marijanavujovic92@gmail.com>
2025-02-17 14:14:44 +0100

    CardsParallax images has been improved

--- data/index.ts
+++ data/index.ts
@@ -82,36 +82,36 @@ export const logos = [
 
 export const projects = [
   {
-    title: "Matthias Leidinger",
-    description: "Originally hailing from Austria, Berlin-based photographer Matthias Leindinger is a young creative brimming with talent and ideas.",
+    title: "Kosili",
+    description: "Labels & Packaging",
     src: "/portfolio/labels-packaging/1_chocolate-bar-design.jpg",
     link: "/portfolio/labels-packaging/1_chocolate-bar-design.jpg",
     color: "#BBACAF"
   },
   {
     title: "Clément Chapillon",
-    description: "This is a story on the border between reality and imaginary, about the contradictory feelings that the insularity of a rocky, arid, and wild territory provokes”—so French photographer Clément Chapillon describes his latest highly captivating project Les rochers fauves (French for ‘The tawny rocks’).",
-    src: "/portfolio/labels-packaging/2_product-design-prints.jpg",
+    description: "FLyers & Posters",
+    src: "/portfolio/labels-packaging/10_shelf-ready-packaging.jpg",
     link: "https://www.ignant.com/2022/09/30/clement-chapillon-questions-geographical-and-mental-isolation-with-les-rochers-fauves/",
     color: "#977F6D"
   },
   {
     title: "Zissou",
-    description: "Though he views photography as a medium for storytelling, Zissou’s images don’t insist on a narrative. Both crisp and ethereal, they’re encoded with an ambiguity—a certain tension—that lets the viewer find their own story within them.",
+    description: "FLyers & Posters",
     src: "/portfolio/labels-packaging/3_label-box-packaging-doypack-laundry-detergent.jpg",
     link: "https://www.ignant.com/2023/10/28/capturing-balis-many-faces-zissou-documents-the-sacred-and-the-mundane-of-a-fragile-island/",
     color: "#C2491D"
   },
   {
     title: "Mathias Svold and Ulrik Hasemann",
-    description: "The coastlines of Denmark are documented in tonal colors in a pensive new series by Danish photographers Ulrik Hasemann and Mathias Svold; an ongoing project investigating how humans interact with and disrupt the Danish coast.",
+    description: "FLyers & Posters",
     src: "/portfolio/labels-packaging/4_stand-up-pouch-doypack-laundry-detergent-clothes.jpg",
     link: "https://www.ignant.com/2019/03/13/a-photographic-series-depicting-the-uncertain-future-of-denmarks-treasured-coastlines/",
     color: "#B62429"
   },
   {
     title: "Mark Rammers",
-    description: "Dutch photographer Mark Rammers has shared with IGNANT the first chapter of his latest photographic project, ‘all over again’—captured while in residency at Hektor, an old farm in Los Valles, Lanzarote. Titled ‘Beginnings’, the mesmerizing collection of images is a visual and meditative journey into the origins of regrets and the uncertainty of stepping into new unknowns.",
+    description: "FLyers & Posters",
     src: "/portfolio/labels-packaging/5_box-dieline-packaging-dou-pack.jpg",
     link: "https://www.ignant.com/2023/04/12/mark-rammers-all-over-again-is-a-study-of-regret-and-the-willingness-to-move-forward/",
     color: "#88A28D"

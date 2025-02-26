4281c90cb85632cdffe7b181c27950397c544199
marijanadev <marijanavujovic92@gmail.com>
2025-02-26 01:04:15 +0100

    Intro improvement

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -6,22 +6,15 @@ import Image from 'next/image';
 
 const Intro: React.FC = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center md:items-start py-10 px-5">
-      {/* Left side - Text */}
-      <div className="md:w-1/3 mb-8 md:mb-0 flex justify-center mt-2">
-        <Image
-          src="/hero_image.png" // replace with your image path
-          alt="Sample"
-          width={300}
-          height={300}
-          className="w-auto h-80 rounded-lg transform transition-all duration-700 ease-in-out hover:scale-105"
-        />
-      </div>
+    <div className="flex justify-center py-10 px-5">
 
-      {/* Right side - Image */}
-      <div className="md:w-2/3 text-center md:text-left md:px-5">
-      <TypewriterEffectSmoothDemo />
-      </div>
+        <Image
+          src="/About_Marko.jpg" // replace with your image path
+          alt="Sample"
+          width={2660}
+          height={2000}
+          className="max-w-6xl h-auto rounded-lg transform transition-all duration-700 ease-in-out hover:scale-105"
+        />
     </div>
   );
 };

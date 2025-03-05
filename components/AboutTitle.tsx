1e5d9b2cc8ea66a263b1e0f5445c8351ab7f0c29
marijanadev <marijanavujovic92@gmail.com>
2025-03-05 22:49:14 +0100

    About title - mobile

--- components/AboutTitle.tsx
+++ components/AboutTitle.tsx
@@ -4,7 +4,7 @@ const AboutTitle: React.FC = () => {
   return (
     <div className="relative flex flex-col items-center w-full max-sm:mx-auto">
       {/* White Section (Larger & Centered) */}
-      <div className="bg-white text-black text-center py-4 px-6 sm:px-8 rounded-xl shadow-lg w-full  max-w-5xl max-sm:w-5/6 relative">
+      <div className="bg-white text-black text-center py-4 px-6 sm:px-8 rounded-xl shadow-lg w-full  max-w-5xl max-sm:w-11/12 relative">
         <p className="text-4xl sm:text-9xl pt-6 sm:pt-10 mn-title">About</p>
       </div>
 

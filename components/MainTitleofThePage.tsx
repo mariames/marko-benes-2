d1e5e44b5b4332c7e08f64c36e69ead40aa9b4a9
marijanadev <marijanavujovic92@gmail.com>
2025-03-05 22:33:02 +0100

    Title update

--- components/MainTitleofThePage.tsx
+++ components/MainTitleofThePage.tsx
@@ -5,9 +5,9 @@ interface MainTitleOfThePageProps {
 }
 const MainTitleOfThePage: React.FC<MainTitleOfThePageProps> = ({title}) => {
   return (
-    <div className="relative flex items-center justify-center w-full mx-auto">
+    <div className="relative flex items-center justify-center w-full  mx-auto">
         {/* White Section (Larger & Centered) */}
-        <div className="bg-white text-black text-center py-4 px-6 sm:px-8 rounded-xl shadow-lg w-full max-w-full sm:max-w-5xl relative">
+        <div className="bg-white text-black text-center py-4 px-6 sm:px-8 rounded-xl shadow-lg w-full max-w-full sm:max-w-5xl max-sm:w-5/6 relative">
           <p className="text-4xl sm:text-9xl pt-6 sm:pt-10 mn-title">{title}</p>
         </div>
     </div>

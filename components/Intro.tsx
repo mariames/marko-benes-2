86bcb4adcf6952ec21b1a80225c47b877a065b5c
marijanadev <marijanavujovic92@gmail.com>
2025-03-07 17:11:09 +0100

    MArko image home

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -13,7 +13,7 @@ const Intro: React.FC = () => {
           alt="Sample"
           width={2660}
           height={2000}
-          className="max-w-6xl h-auto rounded-lg transform transition-all duration-700 ease-in-out hover:scale-105"
+          className="max-sm:mx-4 w-full lg:max-w-4xl h-auto rounded-lg transform transition-all duration-700 ease-in-out hover:scale-105"
         />
     </div>
   );

38878cfd964775c891979ef04508b84b3ece618c
marijanadev <marijanavujovic92@gmail.com>
2025-03-07 21:15:52 +0100

    dp

--- components/DesignProcess.tsx
+++ components/DesignProcess.tsx
@@ -8,10 +8,10 @@ const DesignProcess: React.FC = () => {
           “Every project is unique, but the path to great design is built on a clear and thoughtful approach.”
         </p>
       </div>
-      <div className="mt-14 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 max-w-6xl mx-auto">
+      <div className="mt-14 grid grid-cols-1 gap-6 max-w-6xl mx-auto">
         {steps.map((step, index) => (
-          <div key={index} className="bg-black border border-gray-100 p-6 rounded-lg hover:border-purple-900">
-            <p className="text-base 2xl:text-lg text-gray-100 font-semibold uppercase opacity-75">{step.number} {step.title}</p>
+          <div key={index} className="bg-black p-6 rounded-lg">
+            <p className="text-base 2xl:text-xl text-gray-100 font-semibold uppercase opacity-75">{step.number} {step.title}</p>
             <p className="text-gray-400 mt-2 2xl:text-2xl">{step.description}</p>
           </div>
         ))}

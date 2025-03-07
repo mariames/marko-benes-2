f8da7c93668f52d80dd4c7e807e62f59cef7769d
marijanadev <marijanavujovic92@gmail.com>
2025-03-07 22:27:04 +0100

    Portfolio

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -177,7 +177,7 @@ const ServicesGallery: React.FC = () => {
       {filteredServices.map(({ type, images }) => (
         <div key={type} className="mb-12 mx-4 lg:mx-10">
           {/* ✅ Show Category Title */}
-          <p className="text-lg lg:text-3xl font-normal mb-4 border-b-2 border-gray-700 pb-2 text-white">{type}</p>
+          <p className="text-lg lg:text-3xl 2xl:text-4xl font-normal mb-4 border-b-2 border-gray-700 pb-2 text-white">{type}</p>
 
           {/* ✅ Display Images */}
           <div className="grid grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-10">

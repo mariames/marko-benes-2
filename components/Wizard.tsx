da7612185d0cee09fb3cc7191cfe9e697f31472f
marijanadev <marijanavujovic92@gmail.com>
2025-03-11 13:05:22 +0100

    Use state added

--- components/Wizard.tsx
+++ components/Wizard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect } from 'react';
 import { FaRocket, FaTools, FaPen, FaDollarSign, FaClipboardList } from 'react-icons/fa'; // Import icons from React Icons
 import Field from './Field'; 

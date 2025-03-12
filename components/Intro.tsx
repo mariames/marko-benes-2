2ef5d8b7c33323b412d845f7032dc0cc1e23c6db
marijanadev <marijanavujovic92@gmail.com>
2025-03-12 13:01:43 +0100

    Proba

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -3,19 +3,24 @@
 import React from 'react';
 import { TypewriterEffectSmoothDemo } from './TypewriterEffectSmoothDemo';
 import Image from 'next/image';
+import HorizontalScrollCard from './HorizontalScrollCard';
 
 const Intro: React.FC = () => {
   return (
-    <div className="flex justify-center py-10 px-5">
-
-        <Image
-          src="/About_Marko.jpg" // replace with your image path
-          alt="Sample"
-          width={2660}
-          height={2000}
-          className="max-sm:mx-4 w-full lg:max-w-4xl 2xl:w-full h-auto rounded-lg transform transition-all duration-700 ease-in-out hover:scale-105"
-        />
-    </div>
+    <>
+      <TypewriterEffectSmoothDemo />
+      <div className="flex justify-center py-10 px-5">
+          
+          <Image
+            src="/About_Marko.jpg" // replace with your image path
+            alt="Sample"
+            width={2660}
+            height={2000}
+            className="max-sm:mx-4 w-full lg:max-w-6xl 2xl:w-full h-auto rounded-lg transform transition-all duration-700 ease-in-out hover:scale-105"
+          />
+      </div>
+      <HorizontalScrollCard />
+    </>
   );
 };
 
--- components/TypewriterEffectSmoothDemo.tsx
+++ components/TypewriterEffectSmoothDemo.tsx
@@ -19,13 +19,13 @@ export function TypewriterEffectSmoothDemo() {
     },
   ];
   return (
-    <div className="flex flex-col items-center justify-center h-[12rem]">
+    <div className="flex flex-col items-center justify-center h-[12rem] mt-3">
       
-      <p className="farmhouse-title text-5xl mb-5">Hi! I am Marko!</p>  
-      <p className="text-neutral-800 dark:text-neutral-200 font-semibold text-xs sm:text-base md:text-xl lg:text:2xl xl:text-2xl ">
-        A <span className="text-gray-600">Graphic Designer</span> specializing in creating delightful designs.
+      <p className="farmhouse-title text-5xl mb-5 text-neutral-100">Hi! I am Marko!</p>  
+      <p className="text-neutral-100 dark:text-neutral-200 font-semibold text-xs sm:text-base md:text-xl lg:text:2xl xl:text-2xl ">
+        A <span className="text-gray-400">Graphic Designer</span> specializing in creating delightful designs.
       </p>
-      <p className="mb-5 text-neutral-800 dark:text-neutral-200 font-semibold text-xs sm:text-base md:text-xl lg:text:2xl xl:text-2xl ">
+      <p className="mb-5 text-neutral-100 dark:text-neutral-200 font-semibold text-xs sm:text-base md:text-xl lg:text:2xl xl:text-2xl ">
         Partnering with agencies, start-ups, and companies worldwide.
       </p>
     </div>

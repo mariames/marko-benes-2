81ddbcfd0232ad42b186101ee0fcff8d3440a5d1
marijanadev <marijanavujovic92@gmail.com>
2025-04-14 12:41:48 +0200

    AnimatedCard

--- components/AnimatedCard/AnimatedCard.tsx
+++ components/AnimatedCard/AnimatedCard.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 
 const AnimatedCard = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-black">
-      <div className="relative w-full px-7">
+    <div className="flex justify-center items-center bg-black">
+      <div className="relative w-full px-7 4xl:mx-52">
         <Image
           src="/about/6406_frellance-carrier.jpg"
           alt="The Great Path"
@@ -14,11 +14,11 @@ const AnimatedCard = () => {
           height={1679}
         />
 
-        <div className="card-data absolute left-1/2 transform -translate-x-1/2 bottom-[-9rem] w-[680px] bg-gray-50 rounded-xl p-10 shadow-lg opacity-0 transition-all duration-1000">
-          <p className="sm:text-2xl md:text-4xl 2xl:text-5xl 3xl:text-5xl 4xl:text-7xl 5xl:text-9xl  font-medium farmhouse-title text-black mb-4">
+        <div className="card-data absolute left-1/2 transform -translate-x-1/2 bottom-[-9rem] w-[680px] 2xl:w-[800px] 3xl:w-[900px] 4xl:w-[1600px] bg-gray-50 rounded-xl 4xl:p-20 shadow-lg opacity-0 transition-all duration-1000">
+          <p className="sm:text-2xl md:text-4xl 2xl:text-5xl 3xl:text-5xl 4xl:text-7xl 5xl:text-9xl  font-medium farmhouse-title text-black mb-4 text-center">
             Hi! I am Marko!
           </p>
-          <p className="text-xl font-medium text-gray-800 mb-2">
+          <p className="text-xl font-medium text-gray-800 mb-2 4xl:text-5xl text-center">
             A graphic designer specializing in creating delightful designs.
           </p>
         </div>

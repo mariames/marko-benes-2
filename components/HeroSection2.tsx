b1cfe5599a29b174d6be560b4d6a7843fdf5aafc
marijanadev <marijanavujovic92@gmail.com>
2025-04-15 11:40:54 +0200

    Hs 2

--- components/HeroSection2.tsx
+++ components/HeroSection2.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 const HeroSection2 = () => {
   return (
     <>
+        {/* 
         <div className='pt-10 px-4 md:px-10 4xl:px-56'>
         <Image
             src="/about/6406_frellance-carrier.jpg"
@@ -13,7 +14,8 @@ const HeroSection2 = () => {
             height={1679}
             />
         </div>
-        {/* 
+        */}
+        
         <div className='pt-32'>
         <span className="sm:text-2xl md:text-4xl 2xl:text-5xl 3xl:text-5xl 4xl:text-7xl 5xl:text-9xl  font-medium farmhouse-title text-white mb-4 text-center flex justify-center">
             Hi! I am Marko!
@@ -22,7 +24,6 @@ const HeroSection2 = () => {
             A graphic designer specializing in creating delightful designs.
           </p>
         </div>
-        */}
     </>
   )
 }

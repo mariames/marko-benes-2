7caab0ae64867e340f988cea0cb4d385955b141d
marijanadev <marijanavujovic92@gmail.com>
2025-04-15 09:02:43 +0200

    AnimatedCard

--- components/AnimatedCard/AnimatedCard.tsx
+++ components/AnimatedCard/AnimatedCard.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import './AnimatedCard.css'; // We'll write minimal CSS for animation separately
-import Image from 'next/image';
+import Image from "next/image";
+import React from "react";
 
 const AnimatedCard = () => {
   return (
-    <div className="flex justify-center items-center bg-black">
-      <div className="relative w-full px-7 4xl:mx-52">
+    <div className="flex justify-center items-center min-h-screen bg-black px-4">
+      <div className="relative group w-full">
+        {/* Image */}
         <Image
           src="/about/6406_frellance-carrier.jpg"
           alt="The Great Path"
@@ -14,10 +14,22 @@ const AnimatedCard = () => {
           height={1679}
         />
 
-        <div className="card-data absolute left-1/2 transform -translate-x-1/2 bottom-[-16rem] w-[680px] 2xl:w-[800px] 3xl:w-[900px] 4xl:w-[1800px] 5xl:w-[2800px] bg-gray-50 rounded-xl 4xl:p-24 shadow-lg opacity-95 transition-all duration-1000 ">
-          <p className="sm:text-2xl md:text-4xl 2xl:text-5xl 3xl:text-5xl 4xl:text-7xl 5xl:text-9xl  font-medium farmhouse-title text-black mb-4 text-center">
+        {/* Hidden content that appears on hover */}
+        <div
+          className="
+            absolute left-0 right-0 mx-auto
+            bg-white rounded-xl shadow-lg
+            md:w-[680px] sm:w-[316px]
+            px-6 py-7
+            opacity-0 group-hover:opacity-100
+            translate-y-32 group-hover:translate-y-[-7rem]
+            transition-all duration-700
+            z-10
+          "
+        >
+          <span className="sm:text-2xl md:text-4xl 2xl:text-5xl 3xl:text-5xl 4xl:text-7xl 5xl:text-9xl  font-medium farmhouse-title text-black mb-4 text-center">
             Hi! I am Marko!
-          </p>
+          </span>
           <p className="text-xl font-semibold text-gray-800 mb-2 4xl:text-7xl text-center popins-title">
             A graphic designer specializing in creating delightful designs.
           </p>

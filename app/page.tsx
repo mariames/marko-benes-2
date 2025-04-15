d0d64ff314495e39f7c553a6c045b63f519f7e43
marijanadev <marijanavujovic92@gmail.com>
2025-04-15 09:13:43 +0200

    Check screen

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import Logos from "@/components/Logos";
 import Services from "@/components/Services";
 import CallToAction from "@/components/CallToAction";
 import CustomCursor from "@/components/CustomCursor/CustomCursor";
-import AnimatedCard from "@/components/AnimatedCard/AnimatedCard";
+//import AnimatedCard from "@/components/AnimatedCard/AnimatedCard";
 
 export default function Home() {
   return (
     <main>
       <CustomCursor />
-      <AnimatedCard />
+      {/*}<AnimatedCard />*/}
       <div className="h-96"></div>
       <Services />
       <Logos />

13cd0b0dbf294dc86fb14c16ce3d51c7015842b6
marijanadev <marijanavujovic92@gmail.com>
2025-04-15 14:01:46 +0200

    Labels pa

--- app/services/labels_and_packaging/page.tsx
+++ app/services/labels_and_packaging/page.tsx
@@ -86,7 +86,7 @@ const page = () => {
         rightImgs={rightImgs}
         portfolioLink="/portfolio"/>
 
-      <div className='mx-3 my-40'>
+      <div className='mx-3 my-40 4xl:mx-20'>
         <TextBlock>
           The key goal of every design is to strengthen brand identity and capture consumer attention on the shelf. That&#39;s why I develop every packaging and label design strategically, considering all essential elements such as striking visuals, customized backgrounds, carefully positioned text, well-designed icons, and a harmonious color palette. This ensures that the final product is visually powerful, recognizable, and flawlessly executed in every aspect—from concept to the finished product.
         </TextBlock>
@@ -98,14 +98,15 @@ const page = () => {
         </TextBlock>
       </div>
 
+      <AnimatedLabels labels={labels} direction="left-to-right" />
+      <AnimatedLabels labels={labels} direction="right-to-left" />
+
         <TextInfo />
 
 
         <PrintTechniques />
 
 
-        <AnimatedLabels labels={labels} direction="left-to-right" />
-        <AnimatedLabels labels={labels} direction="right-to-left" />
 
       
         <SummaryCard 
--- components/PrintTechniques.tsx
+++ components/PrintTechniques.tsx
@@ -19,7 +19,7 @@ const PrintTechniques: React.FC = () => {
             transition={{ duration: 1 }}
             viewport={{ once: true }}
           >
-            <Image src="/service-section/LabelsAndPackaging/print_finishing_techniques/dry_stamp_embossing_debossing_label_design.jpg" width={1400} height={972} alt="Promotional Design" className="w-full h-auto 4xl:h-[500] 4xl:w-auto rounded-lg" />
+            <Image src="/service-section/LabelsAndPackaging/print_finishing_techniques/dry_stamp_embossing_debossing_label_design.jpg" width={1400} height={972} alt="Promotional Design" className="w-full h-auto rounded-lg" />
           </motion.div>
           <motion.div
             className="flex-1"

364a2af23f3ba078dd39de0dc4acb797ac179215
marijanadev <marijanavujovic92@gmail.com>
2025-04-16 19:40:00 +0200

    resp

--- components/ScrollImages.tsx
+++ components/ScrollImages.tsx
@@ -12,11 +12,11 @@ const ScrollImages: React.FC<ScrollImagesProps> = ({ images, direction }) => {
   const imageRow = [...images, ...images, ...images]; // tripled for looping
 
   return (
-    <div className="relative overflow-hidden w-full h-auto">
-        <p className="text-3xl text-white"> HELLO WORD </p>
+    <div className="relative overflow-hidden w-full h-44 bg-purple-500">
+        
         
       <motion.div
-        className="absolute left-0 top-0 flex flex-nowrap gap-4"
+        className="absolute left-0 top-0 flex flex-nowrap gap-2"
         style={{ willChange: "transform" }}
         animate={{
           x: isLeftToRight ? ["-33.33%", "0%"] : ["0%", "-33.33%"],
@@ -28,11 +28,11 @@ const ScrollImages: React.FC<ScrollImagesProps> = ({ images, direction }) => {
         }}
       >
         {imageRow.map((item, index) => (
-          <div key={index} className="min-w-[300px] h-[300px] flex-shrink-0 bg-pink-500">
+          <div key={index} className="min-w-[176px] h-[176px] flex-shrink-0 bg-pink-500">
             <img
               src={item.img}
               alt={item.name}
-              className="w-32 h-auto object-cover rounded-lg"
+              className="w-44 h-auto object-cover rounded-lg"
             />
           </div>
         ))}

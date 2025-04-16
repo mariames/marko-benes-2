43b671ff1e931dcc79cd8342168ad69820d7bd2b
marijanadev <marijanavujovic92@gmail.com>
2025-04-16 20:41:21 +0200

    Responsive

--- components/ResponsiveServiceHero.tsx
+++ components/ResponsiveServiceHero.tsx
@@ -54,7 +54,7 @@ const ResponsiveServiceHero: React.FC<ServiceHeroProps> = ({
           <AnimatedButton text="Got a project?" link="/contact" />
           <motion.a
             href={portfolioLink}
-            className="px-1 py-1 lg:px-6 lg:py-3 border border-white rounded-lg text-white"
+            className="px-1 py-2 lg:px-6 lg:py-3 border border-white rounded-lg text-white"
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
           >

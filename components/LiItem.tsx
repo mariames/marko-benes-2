c3d1b21caa1863b473206cf801a9676d7a9bbb03
marijanadev <marijanavujovic92@gmail.com>
2025-04-17 16:46:35 +0200

    Li  Center

--- components/LiItem.tsx
+++ components/LiItem.tsx
@@ -8,7 +8,7 @@ interface LiItemProps {
 const LiItem: React.FC<LiItemProps> = ({ children }) => {
   return (
     <div className="flex items-start space-x-3 mt-2">
-        <div className="pt-1">
+        <div className="flex self-center">
             <FaCheckCircle className="text-[#e5dfd3] w-3 h-3 lg:w-5 lg:h-5 2xl:w-8 2xl:h-8" />
         </div>
         <p className="text-gray-300 2xl:text-2xl">

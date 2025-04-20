4cd262698ef301a3f8c42e68d727f36727651f03
marijanadev <marijanavujovic92@gmail.com>
2025-04-21 01:13:51 +0200

    resp

--- components/HeroSection2.tsx
+++ components/HeroSection2.tsx
@@ -12,13 +12,13 @@ const HeroSection2 = () => {
             height={1679}
             />
         
-        <div className='pt-32'>
-        <span className="text-2xl md:text-4xl 2xl:text-5xl 3xl:text-5xl 4xl:text-7xl 5xl:text-9xl  font-medium farmhouse-title text-white mb-4 text-center flex justify-center">
-            Hi! I am Marko!
-          </span>
-          <p className="text-xl font-semibold text-white mb-2 4xl:text-7xl text-center popins-title">
-            A graphic designer specializing in creating delightful designs.
-          </p>
+        <div className='pt-16 lg:pt-32'>
+          <span className="text-2xl md:text-4xl 2xl:text-5xl 3xl:text-5xl 4xl:text-7xl 5xl:text-9xl  font-medium farmhouse-title text-white mb-4 text-center flex justify-center">
+              Hi! I am Marko!
+            </span>
+            <p className="text-xl font-semibold text-white mb-2 4xl:text-7xl text-center popins-title">
+              A graphic designer specializing in creating delightful designs.
+            </p>
         </div>
     </>
   )

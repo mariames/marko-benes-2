753583c40a7b91f91d1bda446d3aeda8e42519d5
marijanadev <marijanavujovic92@gmail.com>
2025-04-22 23:10:37 +0200

    Update

--- components/HeroSection2.tsx
+++ components/HeroSection2.tsx
@@ -4,22 +4,25 @@ import React from 'react'
 const HeroSection2 = () => {
   return (
     <>
+      <div className="max-w-full px-4 mx-auto">
         <Image
-            src="/about/6406_frellance-carrier.jpg"
-            alt="Marko Benis"
-            className="rounded-2xl w-full h-auto"
-            width={3840}
-            height={1679}
-            />
-        
-        <div className='pt-16 lg:pt-32'>
-          <span className="text-2xl md:text-4xl 2xl:text-5xl 3xl:text-5xl 4xl:text-7xl 5xl:text-9xl  font-medium farmhouse-title text-white mb-4 text-center flex justify-center">
-              Hi! I am Marko!
-            </span>
-            <p className="text-xl font-semibold text-white mb-2 4xl:text-7xl text-center popins-title">
-              A graphic designer specializing in creating delightful designs.
-            </p>
+          src="/about/6406_frellance-carrier.jpg"
+          alt="Marko Benis"
+          className="rounded-2xl w-full h-auto"
+          width={3840}
+          height={1679}
+          priority
+        />
+
+        <div className="pt-16 lg:pt-32 text-center">
+          <span className="block text-2xl md:text-4xl xl:text-5xl font-medium farmhouse-title text-white mb-4">
+            Hi! I am Marko!
+          </span>
+          <p className="text-xl font-semibold text-white mb-2 xl:text-3xl popins-title">
+            A graphic designer specializing in creating delightful designs.
+          </p>
         </div>
+      </div>
     </>
   )
 }

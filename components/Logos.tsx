ef4291f3e599621e6bcc6ce46673d7628b7d2b17
marijanadev <marijanavujovic92@gmail.com>
2025-04-23 00:40:29 +0200

    Logos upd

--- components/Logos.tsx
+++ components/Logos.tsx
@@ -83,7 +83,7 @@ const Logos = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const checkMobile = () => setIsMobile(window.innerWidth < 640);
+    const checkMobile = () => setIsMobile(window.innerWidth < 800);
     checkMobile();
     window.addEventListener("resize", checkMobile);
     return () => window.removeEventListener("resize", checkMobile);

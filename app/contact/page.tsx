1394513b77e1cda47a141b976a033ec1d4d7e786
marijanadev <marijanavujovic92@gmail.com>
2025-04-29 00:13:17 +0200

    Update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -10,7 +10,7 @@ export default function Contact() {
         smallTitle="PROJECT PLANNER"
         smallMessage="LETS TALK ABOUT PROJECT" />
         
-        <div className="flex justify-center">
+        <div className="flex justify-center text-center">
           <p className='text-white text-lg md:text-xl 2xl:text-3xl 4xl:text-4xl'>Submit the planner form or email me at marko@benis.studio.</p>
         </div>
       <ProjectPlaner />        

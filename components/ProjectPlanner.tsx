24fd356cc41c8d799a9d228f19aa873ae0a316d9
marijanadev <marijanavujovic92@gmail.com>
2025-04-29 00:26:38 +0200

    Pr

--- components/ProjectPlanner.tsx
+++ components/ProjectPlanner.tsx
@@ -1,26 +1,17 @@
 "use client";
 import React from "react";
 import Wizard from "./Wizard";
-import { motion } from "framer-motion"; // Import Framer Motion
 
 
 const ProjectPlaner: React.FC = () => {
 
   return (
     <section className="bg-black text-white py-10 px-6">
+
       <div className="w-full md:max-w-3xl mx-auto">
         
-
-        {/* Tabs Content */}
-        <motion.div
-          key={"activeTab"}
-          initial={{ opacity: 0, x: 100 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: -100 }}
-          transition={{ duration: 0.5, ease: "easeOut" }}
-        >
           <Wizard />
-        </motion.div>
+
       </div>
     </section>
   );

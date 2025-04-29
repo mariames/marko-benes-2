8cd1c96f56bf41f85f89638a29c867f2d095df30
marijanadev <marijanavujovic92@gmail.com>
2025-04-29 12:32:42 +0200

    resp

--- components/HeroSection2.tsx
+++ components/HeroSection2.tsx
@@ -18,7 +18,7 @@ const HeroSection2 = () => {
           <span className="block text-4xl md:text-4xl xl:text-5xl 4xl:text-7xl 5xl:text-8xl font-medium farmhouse-title text-white mb-4">
             Hi! I am Marko!
           </span>
-          <p className="text-xl font-semibold text-white mb-2 xl:text-3xl 4xl:text-7xl 5xl:text-8xl popins-title">
+          <p className="text-xl font-semibold text-white mb-2 xl:text-3xl 4xl:text-6xl 5xl:text-8xl popins-title">
             A graphic designer specializing in creating delightful designs.
           </p>
         </div>

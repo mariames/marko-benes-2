e9dc0e72846442deac9441dd0ab8e749b4524c59
marijanadev <marijanavujovic92@gmail.com>
2025-04-29 12:56:52 +0200

    Print Technique

--- components/PrintTechniques.tsx
+++ components/PrintTechniques.tsx
@@ -8,7 +8,7 @@ import UvVarnish from "./UvVarnish";
 
 const PrintTechniques: React.FC = () => {
   return (
-    <section className="bg-black text-white py-10 lg:py-16 relative flex justify-center w-full overflow-x-hidden">
+    <section className="bg-black text-white py-10 lg:py-16 relative flex justify-center w-full overflow-x-hidden max-sm:overflow-y-hidden">
       <div className="w-full max-w-screen-xl 4xl:max-w-screen-3xl space-y-12">
         
         {/* First Row: Image on the left, text on the right */}

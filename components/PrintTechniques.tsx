8dbcc02dc5f07b1adbb06544d5b16781b21f3b5c
marijanadev <marijanavujovic92@gmail.com>
2025-05-01 13:53:25 +0200

    update

--- components/PrintTechniques.tsx
+++ components/PrintTechniques.tsx
@@ -11,7 +11,7 @@ const PrintTechniques: React.FC = () => {
     <section className="bg-black text-white py-10 lg:py-16 relative flex justify-center w-full overflow-x-hidden max-sm:overflow-y-hidden">
       <div className="w-full max-w-screen-xl 4xl:max-w-screen-3xl space-y-12">
         
-        {/* First Row: Image on the left, text on the right */}
+        {/* DRY STAMP */}
         <div className="flex flex-col lg:flex-row gap-4 items-center">
           {/* 
           <motion.div
@@ -32,8 +32,9 @@ const PrintTechniques: React.FC = () => {
           </motion.div>          
           
           */}
+          <div className="flex-1">
           <motion.img
-            className="flex-1 w-72 lg:w-full h-auto rounded-lg"
+            className="w-72 lg:w-full h-auto rounded-lg"
             initial={{ x: -100, opacity: 0 }}
             whileInView={{ x: 0, opacity: 1 }}
             transition={{ duration: 1 }}
@@ -41,8 +42,9 @@ const PrintTechniques: React.FC = () => {
             src="/service-section/LabelsAndPackaging/print_finishing_techniques/dry_stamp_embossing_debossing_label_design.jpg" 
               width={1400} 
               height={972} 
-              alt="Promotional Design" 
+              alt="Dry Stamp" 
           />
+          </div>
           <motion.div
             className="flex-1"
             initial={{ x: 100, opacity: 0 }}
@@ -54,7 +56,7 @@ const PrintTechniques: React.FC = () => {
           </motion.div>
         </div>
 
-        {/* Second Row: Two images */}
+        {/* FOIL STAMPING */}
         <div className="flex flex-col-reverse lg:flex-row items-center">
           <motion.div
             className="flex-1" 
@@ -65,7 +67,7 @@ const PrintTechniques: React.FC = () => {
           >
             <FoilStamping />
           </motion.div>
-
+          {/* 
           <motion.div 
             className="flex-1"
             initial={{ x: 100, opacity: 0 }} 
@@ -81,10 +83,27 @@ const PrintTechniques: React.FC = () => {
               height={972} 
             />
           </motion.div>
+          */}
+          <div className="flex-1">
+            <motion.img 
+              className="w-72 lg:w-full h-auto rounded-lg"
+              initial={{ x: 100, opacity: 0 }} 
+              whileInView={{ x: 0, opacity: 1 }} 
+              transition={{ duration: 1 }} 
+              viewport={{ once: true }}
+              src="/service-section/LabelsAndPackaging/print_finishing_techniques/hot_foil_stamping_foil_blocking_packaging_design.jpg" 
+              alt="Foil Stamping" 
+              width={1400} 
+              height={972} />
+          </div>
+           
+          
+          
         </div>
 
-        {/* Third Row: Image on the left, text on the right */}
+        {/* UV VARNISH */}
         <div className="flex flex-col lg:flex-row gap-4 items-center">
+          {/** 
           <motion.div
             className="flex-1"
             initial={{ x: -100, opacity: 0 }}
@@ -100,6 +119,20 @@ const PrintTechniques: React.FC = () => {
               className="w-72 lg:w-full h-auto rounded-lg" 
             />
           </motion.div>
+          */}
+          <div className="flex-1">
+            <motion.img
+              className="w-72 lg:w-full h-auto rounded-lg"
+              initial={{ x: -100, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1 }}
+              viewport={{ once: true }}
+              src="/service-section/LabelsAndPackaging/print_finishing_techniques/uv_varnish_uv_coating_spot_label_design.jpg" 
+              width={1400} 
+              height={972} 
+              alt="Design Variety" 
+            /> 
+          </div>
           <motion.div
             className="flex-1"
             initial={{ x: 100, opacity: 0 }}

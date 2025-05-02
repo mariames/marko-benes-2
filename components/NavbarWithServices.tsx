c3c97a47512e60cc9b319eba41f9e01989304b6a
marijanadev <marijanavujovic92@gmail.com>
2025-05-02 16:07:39 +0200

    update

--- components/NavbarWithServices.tsx
+++ components/NavbarWithServices.tsx
@@ -54,7 +54,7 @@ export default function NavbarWithServices() {
                                         xmlns="http://www.w3.org/2000/svg"
                                         className="w-6 h-6"
                                         viewBox="0 0 20 20"
-                                        fill="currentColor"
+                                        fill="#E5E7EB"
                                     >
                                         <path
                                             fillRule="evenodd"
@@ -68,7 +68,7 @@ export default function NavbarWithServices() {
                                         className="w-6 h-6"
                                         fill="none"
                                         viewBox="0 0 24 24"
-                                        stroke="currentColor"
+                                        stroke="#E5E7EB"
                                         strokeWidth={2}
                                     >
                                         <path

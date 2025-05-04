7fb955b315174fa42452b3c9dd9c41b06d6f9280
marijanadev <marijanavujovic92@gmail.com>
2025-05-04 18:18:51 +0200

    Update

--- components/HeroSection2.tsx
+++ components/HeroSection2.tsx
@@ -15,11 +15,28 @@ const HeroSection2 = () => {
           priority
         />
         */}
+        {/** 
         <img
           src="/about/6406_frellance-carrier.jpg"
           alt="Marko Benis"
           className="rounded-2xl w-full h-auto"
         />
+        */}
+        <div className="relative w-full aspect-[21/9] rounded-2xl overflow-hidden">
+          <img
+            src="/about/6406_frellance-carrier-large.jpg"
+            srcSet="
+              /about/6406_frellance-carrier-small.jpg 960w,
+              /about/6406_frellance-carrier-medium.jpg 2047w,
+              /about/6406_frellance-carrier-large.jpg 3840w
+            "
+            sizes="(max-width: 640px) 100vw, (max-width: 1024px) 80vw, 60vw"
+            alt="Marko Benis"
+            className="absolute inset-0 w-full h-full object-cover"
+            loading="lazy"
+          />
+        </div>
+
 
         <div className="pt-16 lg:pt-32 text-center">
           <span className="block text-4xl md:text-4xl xl:text-5xl 4xl:text-8xl 5xl:text-8xl font-medium farmhouse-title text-white mb-4">

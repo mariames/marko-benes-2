e4ae19ffc8fc3317f7d9bbd2d97c47d7b2bb4896
marijanadev <marijanavujovic92@gmail.com>
2025-05-05 10:46:29 +0200

    Update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -16,7 +16,7 @@ const About = () => {
           smallMessage="DESIGN AS A CALLING" />
         <AboutMarko />
       {/** <LandingSection /> */}
-      <CardSection />
+      {/** <CardSection /> */}
       <Stack />
       <CallToAction />
     </>
